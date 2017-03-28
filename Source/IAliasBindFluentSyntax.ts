60c428e0bbf469c83c4e2a618997a89ecb7aa909
Francisco Mercedes <franciscomerdot@gmail.com>
2017-03-27 21:44:51 -0400

    Missing commit.

--- Source/IAliasBindFluentSyntax.ts
+++ Source/IAliasBindFluentSyntax.ts
@@ -1,5 +1,7 @@
 import { IAliasReference } from "./IAliasReference";
 import { IKernelReference } from "./IKernelReference";
+import { IServingContextFluentSyntax } from "./IServingContextFluentSyntax";
+import { IContainerFluentSyntax } from "./IContainerFluentSyntax";
 
 /**
  * Represents an alias fluent context that allows the kernel register types and objects in a fluent api syntax.
@@ -10,17 +12,17 @@ export interface IAliasBindFluentSyntax extends IAliasReference, IKernelReferenc
      * Register the alias with a instance serving strategy.
      * @param Represents the type to instantiate.
      */
-    To(type:any):void;
+    To(type:any):IServingContextFluentSyntax;
 
     /**
      * Register the alias with a constant serving strategy.
      * @param Represents the object to return.
      */
-    ToConstant(onject:any):void;
+    ToConstant(onject:any):IContainerFluentSyntax;
 
     /**
      * Register the alias with a builder function serving strategy.
      * @param Represents the function that will be invoked to generate the object.
      */
-    ToBuilderFunction(builder:any):void
+    ToBuilderFunction(builder:any):IContainerFluentSyntax;
 }
--- Source/IServingContextFluentSyntax.ts
+++ Source/IServingContextFluentSyntax.ts
@@ -2,4 +2,4 @@ import { ISingeltonFluentSyntax } from "./ISingeltonFluentSyntax";
 import { IContainerFluentSyntax } from "./IContainerFluentSyntax";
 import { IKernelReference } from "./IKernelReference";
 
-export interface IServingFluentContext extends IContainerFluentSyntax, ISingeltonFluentSyntax, IKernelReference { }
+export interface IServingContextFluentSyntax extends IContainerFluentSyntax, ISingeltonFluentSyntax, IKernelReference { }
