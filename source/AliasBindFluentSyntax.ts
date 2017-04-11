cc866a7dc67cd871c18266b5241a90ad33976949
Francisco Mercedes <franciscomerdot@gmail.com>
2017-04-11 17:59:11 -0400

    Start implementing the fluent syntax

---
+++ source/AliasBindFluentSyntax.ts
@@ -0,0 +1,28 @@
+import { IAliasReference } from "./IAliasReference";
+import { IKernelReference } from "./IKernelReference";
+import { IServingContextFluentSyntax } from "./IServingContextFluentSyntax";
+import { IContainerFluentSyntax } from "./IContainerFluentSyntax";
+
+/**
+ * Represents an alias fluent context that allows the kernel register types and objects in a fluent api syntax.
+ */
+export class AliasBindFluentSyntax implements IAliasBindFluentSyntax {
+    
+    /**
+     * Register the alias with a instance serving strategy.
+     * @param Represents the type to instantiate.
+     */
+    To(type:any):IServingContextFluentSyntax;
+
+    /**
+     * Register the alias with a constant serving strategy.
+     * @param Represents the object to return.
+     */
+    ToConstant(onject:any):IContainerFluentSyntax;
+
+    /**
+     * Register the alias with a builder function serving strategy.
+     * @param Represents the function that will be invoked to generate the object.
+     */
+    ToBuilderFunction(builder:any):IContainerFluentSyntax;
+}
