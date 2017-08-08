5b0b5aed12c0f0024b9d3c86b7bfb5387e03734e
Francisco Mercedes <franciscomerdot@gmail.com>
2017-08-07 23:55:55 -0400

    Adding activator, start implementing it. 20%

---
+++ source/ContextualActivator.ts
@@ -0,0 +1,40 @@
+/**
+ * Serve as an instantiator repository used to instantiate the build-in components of the library and allow override them. 
+ */
+class ContextualActivator {
+
+    /**
+     * Creates a new instance of the conext activator.
+     */
+    constructor() {
+        this._instantiators = {};
+    }
+
+    /**
+     * Represents a dictionary of instantiators that are keyed by the unique name.
+     */
+    private _instantiators:{ [name:string]:(contextInstace:any, name:string) => any }    
+
+    /**
+     * Set an instantiator for a particular name, if exists is overrided.
+     * @param {string} name Represents the name that identify the instantiator.
+     * @param {(contextInstace:ContextType, name:string) => InstanceType} instantiator Represents the instantiator that create and return the object.
+     */
+    setContextInstantiator<ContextType, InstanceType>(name:string, instantiator:(contextInstace:ContextType, name:string) => InstanceType):void {
+        this._instantiators[name] = instantiator;
+    }
+
+    /**
+     * Get the instantiator for the provided name.
+     * @param {string} name Represents the name that identify the instantiator.
+     * @returns {(contextInstace:ContextType, name:string) => InstanceType} The instantiator that create and return the object.
+     */
+    getContextInstatiator<ContextType, InstanceType>(name:string):(contextInstace:ContextType, name:string) => InstanceType {
+        if (!this._instantiators[name])
+            throw Error(`There is no instantiator with the given name [${name}]`);
+
+        return this._instantiators[name];
+    }
+}
+
+export = new ContextualActivator();
--- source/Index.ts
+++ source/Index.ts
@@ -1,29 +1,33 @@
-/**
- * An implementation of IoC pattern based on dependency injection that allows you to granulate and decouple your libraries or applications.
- */
+import { IKernel } from './IKernel';
+import contextualActivator from './ContextualActivator'
 
-//Exporting accessible build-in Clasess
+export function setContextInstantiator<ContextType, InstanceType>(name:string, instantiator:(contextInstace:ContextType, name:string) => InstanceType):void {
+    contextualActivator.setContextInstantiator<ContextType, InstanceType>(name, instantiator);
+}
 
+export function createKernel():IKernel {
+    return contextualActivator.getContextInstatiator<any, IKernel>('kernel')(this, '');
+}
+
+
+// Exporting accessible build-in Clasess
 export * from './DependencyMetadata'
-export * from './Kernel'
 export * from './ResolutionConfiguration'
 export * from './ServicingStrategy'
 
 //Exporting Innerfaces
-
+export { IKernel as  IKernel}
 export * from './IAliasBindFluentSyntax';
 export * from './IAliasReference';
 export * from './IContainer';
 export * from './IContainerActivator';
 export * from './IContainerFluentSyntax';
 export * from './IIdentifierReference';
-export * from './IKernel';
 export * from './IKernelReference';
 export * from './IModule';
 export * from './IServicingContextFluentSyntax';
 export * from './ISingeltonFluentSyntax';
 
 //Exporting Errors
-
 import * as Errors from './Errors/Index';
 export { Errors as Errors };
