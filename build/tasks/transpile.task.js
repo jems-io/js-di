bad6fa4acd1804dc5d3bc3348b140916230e46a7
Francisco Mercedes <franciscomerdot@gmail.com>
2017-11-19 22:46:28 -0400

    [Fix] exit the source code and test transpilation task when an erros occurs an cleaning folder before.

--- build/tasks/transpile.task.js
+++ build/tasks/transpile.task.js
@@ -1,29 +1,39 @@
 const ts = require('gulp-typescript');
 const merge = require('gulp-merge');
+const clean = require('gulp-clean');
 
 module.exports = function configureGulp(gulp) {
    
-  gulp.task('transpile-source', function () {
+  gulp.task('transpile-source', ['clean-binaries'], function () {
     let tsProject = getTSProject();
+    let hasError = false;
     let result =  gulp.src(['./src/**/*'])
                       .pipe(tsProject())
-                      .on('error', () => this.on("finish", () => process.exit(1)));
+                      .on('error', () => hasError = true)
+                      .on("finish", () => { if (hasError) process.exit(1) });
 
     return merge([result.js.pipe(gulp.dest('./bin/src')),
                   result.dts.pipe(gulp.dest('./bin/src'))])
   });
 
-  gulp.task('transpile-test', function () {
+  gulp.task('transpile-test', ['transpile-source'], function () {
     let tsProject = getTSProject();
-    
+    let hasError = false;
+
     return gulp.src(['./test/**/*'])
                .pipe(tsProject())
-               .on('error', () => this.on("finish", () => process.exit(1)))
+               .on('error', () => hasError = true)
+               .on("finish", () => { if (hasError) process.exit(1) })
                .js
                .pipe(gulp.dest('./bin/test'));
   });
 
-  gulp.task('transpile', ['transpile-source', 'transpile-test']);
+  gulp.task('clean-binaries', function() {
+    return gulp.src('./bin')
+               .pipe(clean());
+  });
+
+  gulp.task('transpile', ['transpile-test']);
 }
 
 function getTSProject() {
